5e079e2eb50a5a17b6559ff67af9eb673349e2c9
DevelopTech365 <cxbfd2023@gmail.com>
2024-10-12 12:12:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,107 @@
-for (let index = 0; index < 4; index++) {
-    music.play(music.stringPlayable("C - C - G - G - ", 120), music.PlaybackMode.UntilDone)
-}
+basic.showString("Hello!")
+datalogger.log(
+datalogger.createCV("5", 1),
+datalogger.createCV("5", 2),
+datalogger.createCV("5", 3),
+datalogger.createCV("5", 4),
+datalogger.createCV("5", 5)
+)
+basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Surprised)
+basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.StickFigure)
+basic.showLeds(`
+    # . # . .
+    . # # # .
+    . . # . #
+    . # . # .
+    # . . . #
+    `)
+basic.showIcon(IconNames.StickFigure)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . . # . .
+    . # . # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . # . #
+    # . # . #
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . . # #
+    # . # . #
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . # # #
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . # . #
+    # . . # #
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . # . #
+    # . # . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . # . #
+    # # . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # # # . #
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    # # . . #
+    # . # . #
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . # . #
+    # . # . #
+    # . . . #
+    . # # # .
+    `)
 basic.forever(function () {
 	
 })
